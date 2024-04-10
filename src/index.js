216bcec26144a8dc68bae22a4c8f0574fcbb8690
m.karenko <m.karenko@outlook.com>
2024-04-10 21:43:39 +0200

    Added GH Pages

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import './theme/tailwind.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
-		<BrowserRouter>
+		<BrowserRouter basename='/home'>
 			<App />
 		</BrowserRouter>
 	</React.StrictMode>
