81ae4ad63b2898546f268fec1120b09055499f88
m.karenko <m.karenko@outlook.com>
2024-04-10 21:47:48 +0200

    Fix for the gh pages

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {BrowserRouter} from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 
 import reportWebVitals from './reportWebVitals';
 import App from './App';
@@ -11,9 +11,9 @@ import './theme/tailwind.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
-		<BrowserRouter basename='/home'>
+		<HashRouter>
 			<App />
-		</BrowserRouter>
+		</HashRouter>
 	</React.StrictMode>
 );
 
